5c41c74fc9b11adde988746f9c1fa33ea73e8a29
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-09-07 11:35:08 +0630

    ProviderIndexFixed

--- resources/views/admin/providers/index.blade.php
+++ resources/views/admin/providers/index.blade.php
@@ -8,17 +8,41 @@
 </head>
 <body>
     <div>
+<table class="table table-bordered table-striped table-hover">
+    <thead class="table-dark">
+        <tr>
+            <th>Provider</th>
+            <th>Product Name</th>
+            <th>Product Title</th>
+            <th>Game List Status</th>
+            <th>Game Type</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
         @foreach ($providers as $item)
-            <div>
-                 <span>{{ $item->provider}} </span>
-                 &nbsp;
-                 <span> {{ $item->game_list_status }} </span>
-                 &nbsp;
-                  <span> {{ $item->game_type}} </span>
-                 &nbsp;
-                  <a href="/admin/provider-index/{{ $item->id }}" > Status Change </a>
-            </div>
+        <tr>
+            <td>{{ $item->provider }}</td>
+            <td>{{ $item->product_name }}</td>
+            <td>{{ $item->product_title }}</td>
+            <td>
+                <span class="badge
+                    {{ $item->game_list_status == '1' ? 'bg-success' : 'bg-danger' }}">
+                    {{ ucfirst($item->game_list_status) }}
+                </span>
+            </td>
+            <td>{{ $item->game_type }}</td>
+            <td>
+                <a href="/admin/provider-index/{{ $item->id }}"
+                   class="btn btn-sm btn-primary">
+                   Status Change
+                </a>
+            </td>
+        </tr>
         @endforeach
+    </tbody>
+</table>
+</table>
 
     </div>
 </body>
